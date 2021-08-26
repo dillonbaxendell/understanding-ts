2fbadbd2b3c8fcdbc2e9acb3e185a1a831ac40cf
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-08-26 15:07:58 -0500

    the unknown type

--- app.ts
+++ app.ts
@@ -1,36 +1,13 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+//We don't know yet what the user will eventually enter, so...
+let userInput: unknown;
+
+let userName: string;
+
+userInput = 5;
+userInput = 'Dillon';
+
+//Since unknown is less flexible than 'any' type, we can't assign userInput to userName
+//because userName needs to be a string. So we check the type of userInput before doing so.
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-
-//With Void as the return type, it deliberately states that this function doesn't 
-//return anything
-function printResult(num: number): void {
-  console.log("Result: " + num);
-}
-
-function addAndHandle(n1: number, n2: number, someFunction: (num: number) => void) {
-    const result = n1 + n2;
-    someFunction(result);
-}
-
-printResult(add(5, 12));
-
-//CombineValues should accept any function that takes two parameters where 
-//each parameter is a number and the function returns a number
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-
-//Since printResult doesn't match the function type we declared above, it 
-//won't compile and it will throw an error
-//* combineValues = printResult;
-
-// combineValues = 5;
-
-console.log(combineValues(8, 8));
-
-// let someValue: undefined;
-
-addAndHandle(10, 20, (result) => {
-    console.log(result);
-});
