24792f8948836eb8173c265d99dd027612e25ae9
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-08-26 11:52:48 -0500

    object types

--- app.ts
+++ app.ts
@@ -1,21 +1,11 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    // if (typeof n1 !== "number" || typeof n2 !== "number") {
-    //     throw new Error('Incorrect input!');
-    // }
-    const result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    } else {
-        return n1 + n2;
-    }
-}
+//This sort of sets a template for person by assigning the types first
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+const person = {
+    name: "Dillon",
+    age: 26
+};
 
-let number1: number;
-number1 = 5; // 5.0 is the same number
-const number2 = 2.8;
-
-const printResult = true;
-let resultPhrase = 'Result is: ';
-
-add(number1, number2, printResult, resultPhrase);
-
+console.log(person.name);
