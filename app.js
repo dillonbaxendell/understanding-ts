1a7b5c01eb79264cfc7932dcfbb7fe3f8a542a37
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-08-26 14:54:13 -0500

    functions as types

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 function add(n1, n2) {
     return n1 + n2;
 }
+//With Void as the return type, it deliberately states that this function doesn't 
+//return anything
 function printResult(num) {
-    console.log('Result: ' + num);
+    console.log("Result: " + num);
 }
-console.log(printResult(add(5, 12)));
+printResult(add(5, 12));
+//CombineValues should accept any function that takes two parameters where 
+//each parameter is a number and the function returns a number
+var combineValues;
+combineValues = add;
+//Since printResult doesn't match the function type we declared above, it 
+//won't compile and it will throw an error
+//* combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(8, 8));
+// let someValue: undefined;
--- app.ts
+++ app.ts
@@ -1,12 +1,27 @@
 function add(n1: number, n2: number) {
-    return n1 + n2;
+  return n1 + n2;
 }
 
-//With Void as the return type, it deliberately states that this function doesn't return anything 
+//With Void as the return type, it deliberately states that this function doesn't 
+//return anything
 function printResult(num: number): void {
-    console.log('Result: ' + num);
+  console.log("Result: " + num);
 }
 
 printResult(add(5, 12));
+
+//CombineValues should accept any function that takes two parameters where 
+//each parameter is a number and the function returns a number
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+//Since printResult doesn't match the function type we declared above, it 
+//won't compile and it will throw an error
+//* combineValues = printResult;
+
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
 
 // let someValue: undefined;
