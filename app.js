98c95400e80264e0eaf9cd099d954df354509cba
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-08-26 12:06:58 -0500

    working with tuples

--- app.js
+++ app.js
@@ -6,8 +6,15 @@
 var person = {
     name: "Dillon",
     age: 26,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    //A Tuple:
+    role: [2, 'author']
 };
+// person.role.push('admin'); // Push is allowed with Tuples
+// person.role[1] = 10;
+//Although the below is essentially the same thing as pushing into it
+//it gets an Error. A Push won't throw an error. Something to be aware of.
+// person.role = [0, 'admin', 'user'];
 var favoriteActivities;
 favoriteActivities = ['Sports'];
 console.log(person.name);
@@ -16,3 +23,6 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     console.log(hobby.toUpperCase());
     // console.log(hobby.map()); // !!! ERROR !!!
 }
+//Tuple Type:
+//[1, 2]
+//Added by TypeScript: Fixed-length array (and fixed Type)
--- app.ts
+++ app.ts
@@ -3,11 +3,25 @@
 //     name: string;
 //     age: number;
 // } = {
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]
+} = {
     name: "Dillon",
     age: 26,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    //A Tuple:
+    role: [2, 'author']
 };
+
+// person.role.push('admin'); // Push is allowed with Tuples
+// person.role[1] = 10;
+
+//Although the below is essentially the same thing as pushing into it
+//it gets an Error. A Push won't throw an error. Something to be aware of.
+// person.role = [0, 'admin', 'user'];
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
@@ -18,3 +32,7 @@ for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
     // console.log(hobby.map()); // !!! ERROR !!!
 }
+
+//Tuple Type:
+//[1, 2]
+//Added by TypeScript: Fixed-length array (and fixed Type)
