da19d929c484cf18697e191e0c3ff55e089de123
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-08-26 11:27:40 -0500

    typescript vs javascript types

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 function add(n1, n2) {
+    console.log(typeof number1);
+    if (typeof n1 !== "number" && typeof n2 !== "number") {
+        throw new Error('Incorrect input!');
+    }
     return n1 + n2;
 }
 var number1 = "5";
